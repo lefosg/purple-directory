dc25e64e16c9ff8d4693aaf48122643ed22c528a
unknown <47362786+lefosg@users.noreply.github.com>
2025-01-27 01:35:25 +0200

    more

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "purple_dir_devops_backend"
+      storage_account_name = "purpledirsa"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+    }
 }
 
 # Configure the Microsoft Azure Provider
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,23 @@ output "vpngw-ip" {
     description = "Public IP of the vnet gateway"
 }
 
+//these ips are the private ips
+output "domain-controller-ip" {
+    value = "${azurerm_network_interface.NIC-domain-controller-vm.*.private_ip_address}"
+    description = "Domain Controller VM private ip address"
+}
+
 output "wazuh-server-ip" {
     value = "${azurerm_network_interface.NIC-wazuh-server-vm.*.private_ip_address}"
     description = "Wazuh Server VM private ip address"
+}
+
+output "linux-workstation-vm01-ip" {
+    value = "${azurerm_network_interface.NIC-linux-workstation-vm01.*.private_ip_address}"
+    description = "Linux Workstation VM private ip address"
+}
+
+output "workstation-windows-vm02-ip" {
+    value = "${azurerm_network_interface.NIC-workstation-windows-vm.*.private_ip_address}"
+    description = "Windows Workstation VM private ip address"
 }
